6dbea284fb83b63d2e628bf7c5e187f114b74264
georgedaviesr3 <86313117+georgedaviesr3@users.noreply.github.com>
2021-07-06 13:40:08 +0100

    CreatePositionFlow main logic complete, only oracle needed

--- workflows/src/main/kotlin/com/template/flows/CreatePositionFlow.kt
+++ workflows/src/main/kotlin/com/template/flows/CreatePositionFlow.kt
@@ -18,41 +18,26 @@ object CreatePositionFlow {
     @StartableByRPC
     class Initiator(
         private val assetTicker: String,
+        private val collateralPosted: Double,
         private val positionSize: Double,
-        private val exchange: Party
+        private val exchange: Party //Is the exchange ever going to change?
     ) : FlowLogic<SignedTransaction>() {
 
-        /** Get current asset price through oracle */
-        private val currentAssetPrice = 40000.0
-
-        /**Readability*/
-        private val initialAssetPrice = currentAssetPrice
-        private val taker = ourIdentity
-
-
-        /**
-         * The progress tracker checkpoints each stage of the flow and outputs the specified messages when each
-         * checkpoint is reached in the code. See the 'progressTracker.currentStep' expressions within the call() function.
-         */
         companion object {
-            object GENERATING_TRANSACTION :
-                ProgressTracker.Step("Generating transaction based on new PerpFuture Contract.")
-
+            object GETTING_ORACLE_PRICE : ProgressTracker.Step("Getting current asset price from the oracle")
+            object GENERATING_TRANSACTION : ProgressTracker.Step("Generating transaction based on new PerpFuture Contract.")
             object VERIFYING_TRANSACTION : ProgressTracker.Step("Verifying contract constraints.")
-
-            //ORACLE HERE??
             object SIGNING_TRANSACTION : ProgressTracker.Step("Signing transaction with our private key.")
             object GATHERING_SIGS : ProgressTracker.Step("Gathering the counterparty's signature.") {
                 override fun childProgressTracker() = CollectSignaturesFlow.tracker()
             }
-
-            //or maybe oracle here
             object FINALISING_TRANSACTION :
                 ProgressTracker.Step("Obtaining notary signature and recording transaction.") {
                 override fun childProgressTracker() = FinalityFlow.tracker()
             }
 
             fun tracker() = ProgressTracker(
+                GETTING_ORACLE_PRICE,
                 GENERATING_TRANSACTION,
                 VERIFYING_TRANSACTION,
                 SIGNING_TRANSACTION,
@@ -60,70 +45,66 @@ object CreatePositionFlow {
                 FINALISING_TRANSACTION
             )
         }
+        override val progressTracker = ProgressTracker()
 
-        override val progressTracker = ProgressTracker() // not sure what this does
-
-        /**Initiating flow logic*/
         @Suspendable
         override fun call(): SignedTransaction {
-
-            // Step 1. Get a reference to the notary service on our network and our key pair.
             val notary = serviceHub.networkMapCache.notaryIdentities[0]
 
-            // Step 2. Compose the futures contract state.
-            progressTracker.currentStep = GENERATING_TRANSACTION
-            val output =
-                PerpFuturesState(assetTicker, currentAssetPrice, initialAssetPrice, positionSize, taker, exchange)
+            // Get current price through oracle
+            progressTracker.currentStep = GETTING_ORACLE_PRICE
+            val currentAssetPrice = 40000.0 // will equal oracle value here
 
-            // Step 3. Create a new TransactionBuilder object.
+            // Compose the futures contract state and new transaction builder object
+            progressTracker.currentStep = GENERATING_TRANSACTION
+            val output = PerpFuturesState(assetTicker, currentAssetPrice, positionSize, collateralPosted, ourIdentity, exchange)
+
             val builder = TransactionBuilder(notary)
-                .addCommand(PerpFuturesContract.Commands.Create(), listOf(taker.owningKey, exchange.owningKey))
+                .addCommand(PerpFuturesContract.Commands.Create(), listOf(ourIdentity.owningKey, exchange.owningKey))
                 .addOutputState(output)
 
-            // Step 4. Verify and sign it with our KeyPair.
+            // Verify and sign it with our KeyPair.
             progressTracker.currentStep = VERIFYING_TRANSACTION
             builder.verify(serviceHub)
-
-            //Step 5. Sign transaction
             progressTracker.currentStep = SIGNING_TRANSACTION
             val ptx = serviceHub.signInitialTransaction(builder)
 
-            // Step 6. Collect the other party's signature using the SignTransactionFlow.
-            //         will only ever be one counterparty
+            // Collect the other party's signature using the SignTransactionFlow.
+            // will only ever be one counterparty (exchange)
             progressTracker.currentStep = GATHERING_SIGS
             val exchangePartySession = initiateFlow(exchange)
             val fullySignedTx =
                 subFlow(CollectSignaturesFlow(ptx, setOf(exchangePartySession), GATHERING_SIGS.childProgressTracker()))
 
-
-            // Step 7. Assuming no exceptions, we can now finalise the transaction
+            // Finalise the tx
             progressTracker.currentStep = FINALISING_TRANSACTION
-            return subFlow(
-                FinalityFlow(
-                    fullySignedTx,
-                    setOf(exchangePartySession),
-                    FINALISING_TRANSACTION.childProgressTracker()
-                )
-            )
+            return subFlow(FinalityFlow(fullySignedTx, setOf(exchangePartySession), FINALISING_TRANSACTION.childProgressTracker()))
         }
     }
 
-    /** Exchange response logic */
+    /** Exchange Response Logic */
     @InitiatedBy(Initiator::class)
     class Acceptor(val exchangePartySession: FlowSession) : FlowLogic<SignedTransaction>() {
-        //Initiated flow now signs tx
+
         @Suspendable
         override fun call(): SignedTransaction {
             val signTransactionFlow = object : SignTransactionFlow(exchangePartySession) {
                 override fun checkTransaction(stx: SignedTransaction) = requireThat {
-                    //Addition checks
-                    val output = stx.tx.outputs.single().data
+                    // Verify tx meets exchange constraints
+                    val output = stx.tx.outputsOfType<PerpFuturesState>().first()
+
                     "Must be a PerpFuture State" using (output is PerpFuturesState)
-                    val perp = output as PerpFuturesState
-                    val totalValue = perp.initialAssetPrice * perp.positionSize
+                    "Only BTC futures are accepted" using(output.assetTicker == "BTC")
+
+                    val totalValue = output.initialAssetPrice * output.positionSize
                     "Total position size must be less than $1m" using (totalValue < 1000000)
+
+                    val leverage = (output.initialAssetPrice * output.positionSize) / output.collateralPosted
+                    "Leverage must be below 5x" using (leverage < 5)
                 }
             }
+
+            //Sign and return tx
             val txId = subFlow(signTransactionFlow).id
             return subFlow(ReceiveFinalityFlow(exchangePartySession, expectedTxId = txId))
         }
